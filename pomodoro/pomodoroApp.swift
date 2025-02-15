de68173a70c49653bb569f2f00b586e53bb6f5d2
宮森 瑠偉 <r.miyamori@eisansystem.jp>
2025-02-15 18:33:26 +0900

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by USER on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomodoroTests/pomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroTests.swift
+//  pomodoroTests
+//
+//  Created by USER on 2025/02/15.
+//
+
+import Testing
+@testable import pomodoro
+
+struct pomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
